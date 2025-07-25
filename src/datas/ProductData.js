55ae70e79f2c758354a843a42377410756272533
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2025-07-25 14:20:55 +0400

    added new url link to json

--- src/datas/ProductData.js
+++ src/datas/ProductData.js
@@ -3,31 +3,31 @@ const ProductData = [
     id: 1,
     name: "Product 1",
     price: 100,
-    image: "https://placehold.co/250",
+    image: "https://picsum.photos/250/250?random=1",
   },
   {
     id: 2,
     name: "Product 2",
     price: 200,
-    image: "https://placehold.co/250",
+    image: "https://picsum.photos/250/250?random=2",
   },
   {
     id: 3,
     name: "Product 3",
     price: 300,
-    image: "https://placehold.co/250",
+    image: "https://picsum.photos/250/250?random=3",
   },
   {
     id: 4,
     name: "Product 4",
     price: 400,
-    image: "https://placehold.co/250",
+    image: "https://picsum.photos/250/250?random=4",
   },
   {
     id: 5,
     name: "Product 5",
     price: 500,
-    image: "https://placehold.co/250",
+    image: "https://picsum.photos/250/250?random=5",
   },
 ];
 
